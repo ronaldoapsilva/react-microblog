ed4cf0885beedacad33fb80f497b40d34e06ed7c
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2025-07-09 23:57:10 -0300

    add page and routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import Container from "react-bootstrap/Container";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
-import Body from "./components/Body";
-import Posts from "./components/Posts";
+import FeedPage from "./pages/FeedPage";
+import ExplorePage from "./pages/ExplorePage";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
 
 
   return (
     <Container fluid className="App">
-      <Header></Header>
-      <Body sidebar>
-        <Posts></Posts>
-      </Body>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<FeedPage/>}></Route>
+          <Route path="/explore" element={<ExplorePage/>}></Route>
+          <Route path="/login" element={<LoginPage/>}></Route>
+          <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
+        </Routes>
+      </BrowserRouter>
     </Container >
   );
 }
