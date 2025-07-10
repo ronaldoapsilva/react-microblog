2cfba456284f47eb373b550a008928a5674ff70f
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2025-07-10 00:09:23 -0300

    chore: add userpage
    this is page with dynamic parameters

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import FeedPage from "./pages/FeedPage";
 import ExplorePage from "./pages/ExplorePage";
+import UserPage from "./pages/UserPage";
 import LoginPage from "./pages/LoginPage";
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<FeedPage/>}></Route>
           <Route path="/explore" element={<ExplorePage/>}></Route>
+          <Route path="/user/:username" element={<UserPage/>}></Route>
           <Route path="/login" element={<LoginPage/>}></Route>
           <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
         </Routes>
