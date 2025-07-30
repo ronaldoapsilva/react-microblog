5f0817626eab473fb7a320cf3ff208b0eeb84de9
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2025-07-30 07:49:17 -0300

    User Page Actions

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,23 +1,62 @@
-import { useState, useEffect } from 'react';
-import Stack from 'react-bootstrap/Stack';
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import Spinner from 'react-bootstrap/Spinner';
-import { useParams } from 'react-router-dom';
+import Stack from 'react-bootstrap/Stack';
+import { useNavigate, useParams } from 'react-router-dom';
 import Body from '../components/Body';
+import Posts from '../components/Posts';
 import TimeAgo from '../components/TimeAgo';
 import { useApi } from '../contexts/ApiProvider';
-import Posts from '../components/Posts';
+import { useFlash } from '../contexts/FlashProvider';
+import { useUser } from '../contexts/UserProvider';
 
 export default function UserPage() {
   const { username } = useParams();
   const [user, setUser] = useState();
   const api = useApi();
+  const [isFollower, setIsFollower] = useState();
+  const { user: loggedInUser } = useUser();
+  const flash = useFlash();
+  const navigate = useNavigate();
+
   useEffect(() => {
     (async () => {
       const response = await api.get('/users/' + username);
-      setUser(response.ok ? response.body : null);
+      if (response.ok) {
+        setUser(response.body);
+        if (response.body.username !== loggedInUser.username) {
+          const follower = await api.get(
+            '/me/following/' + response.body.id);
+          if (follower.status === 204) {
+            setIsFollower(true);
+          }
+          else if (follower.status === 404) {
+            setIsFollower(false);
+          }
+        }
+        else {
+          setIsFollower(null);
+        }
+      }
+      else {
+        setUser(null);
+      }
     })();
-  }, [username, api]);
+  }, [username, api, loggedInUser]);
+
+  const edit = () => {
+    // TODO
+  };
+
+  const follow = () => {
+    // TODO
+  };
+
+  const unfollow = () => {
+    // TODO
+  };
+
   return (
     <Body sidebar>
       {user === undefined ?
@@ -25,7 +64,7 @@ export default function UserPage() {
         :
         <>
           {user === null ?
-            <p>Could not retrive blog posts.</p>
+            <p>User not found.</p>
             :
             <>
               <Stack direction="horizontal" gap={4}>
@@ -38,6 +77,22 @@ export default function UserPage() {
                     <br />
                     Last seen: <TimeAgo isoDate={user.last_seen} />
                   </p>
+
+                  {isFollower === null &&
+                    <Button variant='primary' onClick={edit}>
+                      Edit
+                    </Button>
+                  }
+                  {isFollower === false &&
+                    <Button variant='primary' onClick={follow}>
+                      Follow
+                    </Button>
+                  }
+                  {isFollower === true &&
+                    <Button variant='primary' onClick={unfollow}>
+                      Unfollow
+                    </Button>
+                  }
                 </div>
               </Stack>
               <Posts content={user.id} />
